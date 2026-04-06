38db525b9d45720b67ee091b7012bb0af660de4d
Junhan Kim <junhan115@gmail.com>
2026-04-06 16:21:28 +0900

    Add files via upload

---
+++ code/5주차/5_게임_dbdiagram.sql
@@ -0,0 +1,34 @@
+	-- dbdiagram.io를 위해 수정하기
+	Table UserAccount {
+		user_id INT [pk]
+		email VARCHAR
+		password VARCHAR
+		created_at TIMESTAMP
+	}
+
+	Table Character {
+		character_id INT [pk]
+		user_id INT
+		name VARCHAR
+		level INT
+		class VARCHAR
+	}
+
+	Table Item {
+		item_id INT [pk]
+		name VARCHAR
+		type VARCHAR
+	}
+
+	Table Inventory {
+		character_id INT
+		item_id INT
+		quantity INT
+		indexes {
+			(character_id, item_id) [pk]
+		}
+	}
+
+	Ref: Character.user_id > UserAccount.user_id
+	Ref: Inventory.character_id > Character.character_id
+	Ref: Inventory.item_id > Item.item_id
